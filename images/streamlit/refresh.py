811112ad4242b9a1b94c346ca1e007ddca194a69
William Brannon <wbrannon@media.mit.edu>
2024-09-25 08:34:35 +0000

    progress bars for streamlit db refresh

--- images/streamlit/refresh.py
+++ images/streamlit/refresh.py
@@ -3,7 +3,10 @@
 import os
 import logging
 
+from tqdm import tqdm
+
 import utils as ut
+
 from models import get_db
 from video_text2text import VideoSummary, VideoSuggestions, ClusterShortName
 
@@ -25,21 +28,23 @@ if __name__ == '__main__':
     if os.path.exists(DB_PATH):
         os.remove(DB_PATH)
 
+    logger.info('Setting up backing database')
     db_factory = get_db(DB_PATH)
+    logger.info('Database set up')
 
     with db_factory() as db_session:
         kwargs = {'session': db_session, 'cache_dir': SUMMARY_SUGGEST_CACHE_DIR}
 
         clusters = ClusterShortName(**kwargs)
-        for key in clusters.uncached_keys:
+        for key in tqdm(clusters.uncached_keys, desc='cluster', leave=True):
             clusters.text2text(key)
 
         summaries = VideoSummary(filters={'is_full_documentary': True}, **kwargs)
-        for key in summaries.uncached_keys:
+        for key in tqdm(summaries.uncached_keys, desc='summary', leave=True):
             summaries.text2text(key)
         summaries.text2text(None)
 
         suggestions = VideoSuggestions(filters={'is_full_documentary': True}, **kwargs)
-        for key in suggestions.uncached_keys:
+        for key in tqdm(suggestions.uncached_keys, desc='suggest', leave=True):
             suggestions.text2text(key)
         suggestions.text2text(None)
